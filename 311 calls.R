69baa0c7057706e98e208aa3f1170e502ca0c80d
kiernang <66456229+kiernang@users.noreply.github.com>
2020-06-05 10:45:36 -0500

    Add files via upload

---
+++ 311 calls.R
@@ -0,0 +1,13 @@
+library(tidyr)
+library(dplyr)
+#I've downloaded a list of 311 (City Services) calls from the City of Winnipeg's open data site
+calls311 <- read_csv("~/Downloads/311_Service_Request.csv")
+#Few issues. One: there are spaces in variable names; Two: the coordinates are jumbled.
+# Additionally, I personally find it annoying when variable names are not in lowercase
+names(calls311) <- tolower(names(calls311))
+names(calls311) <- gsub(' ', '_', names(calls311))
+# Now that the variable names are cleaned up, onto cleaning up the coordinate data
+# A quicker way to do this is to just use a nested gsub command
+calls311$location_1 <- gsub('\\)','',gsub('\\(','',calls311$location_1))
+calls311<- calls311 %>%
+  separate(location_1, c('latitude','longitude'), ', ')
