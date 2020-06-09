4c51b2d5f49e478367bd62f11bfcd5c8ec4d0bcd
Kiernan Gange <KiernanGange@Kiernans-Air-2.hitronhub.home>
2020-06-09 18:58:23 -0500

    Added for loop

--- 311 Call Data.R
+++ 311 Call Data.R
@@ -2,6 +2,7 @@ library(tidyr)
 library(dplyr)
 library(wesanderson)
 library(ggplot2)
+library(ggpubr)
 #I've downloaded a list of 311 (City Services) calls from the City of Winnipeg's open data site
 calls311 <- read_csv("~/Downloads/311_Service_Request.csv")
 #Few issues. One: there are spaces in variable names; Two: the coordinates are jumbled.
@@ -28,3 +29,32 @@ tally() %>%
 top_n(5)
 wwplot <- ggplot(will_whyte_most, aes(service_request, n)) + geom_col( fill = "#0072B2") + labs( x = "Type of Complaint", y = "Number of Calls to 311", title = "William Whyte 311 Calls, 2018 - 2019")
 wwplot + theme(axis.text.x = element_text(face = "bold", color = "#993333", size = 8, angle = 20, hjust = 1))
+#We'll cycle back to this later. For now, let's see what are the worst categories for the entire city
+# From W.W. we can see that recycling and garbage make up large groups on their own - going to merge them due to their similarity
+calls311$service_request_new <- calls311$service_request
+calls311$service_request_new[calls311$service_request_new == ("Missed Garbage Collection")] <- "Missed Household Waste Pickup"
+calls311$service_request_new[calls311$service_request_new == ("Missed Recycling Collection")] <- "Missed Household Waste Pickup"
+# Looking at the top categories in the city
+top_city <-calls311 %>%
+group_by(service_request_new) %>%
+tally() %>%
+top_n(5)
+cityplot <- ggplot(top_city, aes(service_request_new, n)) + geom_col( fill = "#0072B2") + labs( x = "Type of Complaint", y = "Number of Calls to 311", title = "City Wide 311 calls, 2018 - 2019")
+cityplot + theme(axis.text.x = element_text(face = "bold", color = "#993333", size = 6, angle = 20, hjust = 1))
+# Going to subset for these categories plus graffiti. To do this I'll use a for loop.
+call_types <- top_city$service_request_new
+call_types <- append(call_types, "Graffiti", after = 5)
+for (i in call_types){ count <- count +1
+  x <- calls311[calls311$service_request_new == i,] %>%
+  group_by(neighbourhood) %>%
+  tally() %>%
+  top_n(5)
+  nameStuff <- gsub(" ","_", i)
+  nameStuff <-tolower(nameStuff)
+  p <- ggplot(x, aes(x$neighbourhood, x$n)) + geom_col( fill = "#0072B2") + labs( x = paste(i, "calls by Neighbourhood"), y = "Number of Calls to 311", title = paste(i, "calls, 2018-2019"))  
+  assign(paste0(nameStuff,"_calls"),x)
+  assign(paste0(nameStuff, "_plot"),p)
+
+}
+
+ggarrange(missed_household_waste_pickup_plot, ggarrange(neighbourhood_liveability_complaint_plot, potholes_plot))
